6c5b2b275d028d1cfff72206a83b107fc0d139c5
yummygyudon <bang2brew@gmail.com>
2023-12-16 16:30:02 +0900

    FEAT : 요구사항에 따른 입력 명령문 상수화

---
+++ src/main/java/oncall/global/message/Ask.java
@@ -0,0 +1,8 @@
+package oncall.global.message;
+
+public abstract class Ask {
+
+    public static final String PERIOD = "비상 근무를 배정할 월과 시작 요일을 입력하세요> ";
+    public static final String COMMON_DAYS_WORKERS = "평일 비상 근무 순번대로 사원 닉네임을 입력하세요> ";
+    public static final String HOLIDAYS_WORKERS = "휴일 비상 근무 순번대로 사원 닉네임을 입력하세요> ";
+}
